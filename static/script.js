7535ef0575e248daec3bf4d1748e9d439a685330
DEVANARAYAN VS <93283856+thexcomrade@users.noreply.github.com>
2025-05-04 17:47:15 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,8 +0,0 @@
-function fetchData() {
-    fetch('/data')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("output").innerText = data.message;
-        })
-        .catch(error => console.error('Error:', error));
-}
